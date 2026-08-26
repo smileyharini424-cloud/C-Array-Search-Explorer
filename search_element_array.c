66aed3b989a19f2d905b29f14503df735879753d
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-25 23:34:54 -0700

    Create search_element_array.c

---
+++ search_element_array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, search;
+    int arr[100];
+    int found = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &search);
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] == search) {
+            printf("Element found at position %d\n", i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if (found == 0) {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
